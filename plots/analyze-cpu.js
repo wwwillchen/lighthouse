8c0c2262730bb793bc011705d778066728f1be44
Will Chen <willchen90@gmail.com>
2017-05-18 11:00:34 -0700

    csv generator

--- plots/analyze-cpu.js
+++ plots/analyze-cpu.js
@@ -25,32 +25,103 @@ const fs = require('fs');
 const path = require('path');
 
 const args = require('yargs')
-  .describe('input', 'path to artifacts from measure.js')
+  .describe('input', 'path to artifacts from measure.js (pass in a comma separated list)')
+  .array('input')
   .argv;
 
 const DevtoolsTimelineModel = require('../lighthouse-core/lib/traces/devtools-timeline-model');
 const constants = require('./constants');
 const utils = require('./utils');
 
-const INPUT_PATH = args['input'] ? path.resolve(__dirname, args['input']) : constants.OUT_PATH;
+const INPUT_PATHS = args['input'] ? getInputPaths(args['input']) : [constants.OUT_PATH];
+const OUT_CSV_PATH = path.resolve(constants.OUT_PATH, 'cpu.csv');
 
-function main () {
-  const allResults = [];
-  fs.readdirSync(INPUT_PATH).forEach(siteDir => {
-    const sitePath = path.resolve(INPUT_PATH, siteDir);
-    if (!utils.isDir(sitePath)) {
-      return;
-    }
-    const siteResult = {
-      site: siteDir,
-      results: analyzeSite(sitePath)
-    };
-    console.log('Analyzed', sitePath, '\n', siteResult.results); // eslint-disable-line no-console
-    allResults.push(siteResult);
+function getInputPaths(inputs) {
+  return inputs.map((p) => path.resolve(__dirname, p));
+}
+
+function main() {
+  if (!utils.isDir(constants.OUT_PATH)) {
+    fs.mkdirSync(constants.OUT_PATH);
+  }
+
+  const crossBatchResults = [];
+  INPUT_PATHS.forEach((inputPath) => {
+    debugger;
+    const batchResults = [];
+    fs.readdirSync(inputPath).forEach(siteDir => {
+      const sitePath = path.resolve(inputPath, siteDir);
+      if (!utils.isDir(sitePath)) {
+        return;
+      }
+      const siteResult = {
+        site: siteDir,
+        results: analyzeSite(sitePath)
+      };
+      console.log('Analyzed', sitePath, '\n', siteResult.results); // eslint-disable-line no-console
+      batchResults.push(siteResult);
+    });
+    crossBatchResults.push({
+      batch: path.basename(inputPath),
+      results: batchResults,
+    });
   });
+  writeToCSV(crossBatchResults);
 }
 
 main();
+
+/**
+ * @typedef {{batch: string, results: !Array<!SiteResults>}}
+ */
+let BatchResults; // eslint-disable-line no-unused-vars
+
+/**
+ *
+ * @param {!!Array<BatchResults>} allResults
+ */
+function writeToCSV(batchResults) {
+  // TODO: make sure it handles missing results properly
+  const headers = new Set();
+  const sitesMap = new Map();
+  // const siteMap = new Map();
+  const siteToken = '$Site$'
+  headers.add(siteToken);
+  // const siteMap = new Map();
+  batchResults.forEach(({
+    batch,
+    results
+  }) => {
+    results.forEach(({
+      site,
+      results
+    }) => {
+      if (!sitesMap.has(site)) {
+        const map = new Map();
+        map.set(siteToken, site);
+        sitesMap.set(site, map);
+      }
+      results.forEach(({
+        runId,
+        totalNonIdleTimeMs
+      }) => {
+        const header = `${batch}$$${runId}`;
+        headers.add(header);
+        const siteMap = sitesMap.get(site);
+        siteMap.set(header, totalNonIdleTimeMs);
+      });
+    });
+  });
+  let outComponents = [Array.from(headers).join(',')];
+  for (const siteMap of sitesMap.values()) {
+    const lineComponents = [];
+    for (const header of headers) {
+      lineComponents.push(siteMap.get(header));
+    }
+    outComponents.push(lineComponents.join(','));
+  }
+  fs.writeFileSync(OUT_CSV_PATH, outComponents.join('\n'));
+}
 
 /**
  * Aggregates all the run results for a particular site.
@@ -77,7 +148,8 @@ function analyzeSite(sitePath) {
  * @return {number}
  */
 function getTotalNonIdleTimeMsFromTrace(tracePath) {
-  const traceEvents = require(tracePath);
+
+  const traceEvents = JSON.parse(fs.readFileSync(tracePath));
   const model = new DevtoolsTimelineModel(traceEvents);
   const tasks = model.timelineModel().mainThreadTasks();
   return tasks.reduce((acc, task) => {
