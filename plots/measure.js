cc59d4d127aa45e2adc47e0e18463088906285c1
Will Chen <chenwilliam@chromium.org>
2017-05-30 15:03:48 -0700

    paul fb

--- plots/measure.js
+++ plots/measure.js
@@ -17,7 +17,7 @@
 'use strict';
 
 /* eslint-disable no-console */
-
+const fs = require('fs');
 const path = require('path');
 const parseURL = require('url').parse;
 
@@ -62,126 +62,27 @@ const assetSaver = require('../lighthouse-core/lib/asset-saver.js');
 const keepFirstRun = args['keep-first-run'] || !args['reuse-chrome'];
 const numberOfRuns = args['n'] || 3;
 
-const SITES = [
-  // Flagship sites
-  'https://nytimes.com',
-  'https://flipkart.com',
-  'http://www.espn.com/',
-  'https://www.washingtonpost.com/pwa/',
-
-  // TTI Tester sites
-  'https://housing.com/in/buy/real-estate-hyderabad',
-  'http://www.npr.org/',
-  'http://www.vevo.com/',
-  'https://weather.com/',
-  'https://www.nasa.gov/',
-  'https://vine.co/',
-  'http://www.booking.com/',
-  'http://www.thestar.com.my',
-  'http://www.58pic.com',
-  'http://www.dawn.com/',
-  'https://www.ebs.in/IPS/',
-
-  // Sourced from: https://en.wikipedia.org/wiki/List_of_most_popular_websites
-  // (http://www.alexa.com/topsites)
-  // Removed adult websites and duplicates (e.g. google int'l websites)
-  // Also removed sites that don't have significant index pages:
-  // "t.co", "popads.net", "onclickads.net", "microsoftonline.com", "onclckds.com", "cnzz.com",
-  // "live.com", "adf.ly", "googleusercontent.com",
-  'https://www.google.com/search?q=flowers',
-  'https://youtube.com',
-  'https://facebook.com',
-  'https://baidu.com',
-  'https://en.wikipedia.org/wiki/Google',
-  'https://yahoo.com',
-  'https://amazon.com',
-  'http://www.qq.com/',
-  'https://taobao.com',
-  'https://vk.com',
-  'https://mobile.twitter.com/ChromeDevTools',
-  'https://www.instagram.com/stephencurry30',
-  'http://www.hao123.cn/',
-  'http://www.sohu.com/',
-  'https://sina.com.cn',
-  'https://reddit.com',
-  'https://linkedin.com',
-  'https://tmall.com',
-  'https://weibo.com',
-  'https://360.cn',
-  'https://yandex.ru',
-  'https://ebay.com',
-  'https://bing.com',
-  'https://msn.com',
-  'https://www.sogou.com/',
-  'https://wordpress.com',
-  'https://microsoft.com',
-  'https://tumblr.com',
-  'https://aliexpress.com',
-  'https://blogspot.com',
-  'https://netflix.com',
-  'https://ok.ru',
-  'https://stackoverflow.com',
-  'https://imgur.com',
-  'https://apple.com',
-  'http://www.naver.com/',
-  'https://mail.ru',
-  'http://www.imdb.com/',
-  'https://office.com',
-  'https://github.com',
-  'https://pinterest.com',
-  'https://paypal.com',
-  'http://www.tianya.cn/',
-  'https://diply.com',
-  'https://twitch.tv',
-  'https://adobe.com',
-  'https://wikia.com',
-  'https://coccoc.com',
-  'https://so.com',
-  'https://fc2.com',
-  'https://www.pixnet.net/',
-  'https://dropbox.com',
-  'https://zhihu.com',
-  'https://whatsapp.com',
-  'https://alibaba.com',
-  'https://ask.com',
-  'https://bbc.com'
-];
-
 function getUrls() {
-  if (args['sites-path']) {
-    return require(path.resolve(__dirname, args['sites-path']));
-  }
-
   if (args['site']) {
     return [args['site']];
   }
 
-  if (args['subset']) {
-    return [
-      'https://en.wikipedia.org/wiki/Google',
-      'https://mobile.twitter.com/ChromeDevTools',
-      'https://www.instagram.com/stephencurry30',
-      'https://amazon.com',
-      'https://nytimes.com',
-      'https://www.google.com/search?q=flowers',
-
-      'https://flipkart.com',
-      'http://www.espn.com/',
-      'https://www.washingtonpost.com/pwa/',
-      'http://www.npr.org/',
-      'http://www.booking.com/',
-      'https://youtube.com',
-      'https://reddit.com',
-      'https://ebay.com',
-      'https://stackoverflow.com',
-      'https://apple.com',
-
-      // Could not run nasa on gin3g
-      'https://www.nasa.gov/',
-    ];
+  if (args['sites-path']) {
+    const sitesPath = path.resolve(__dirname, args['sites-path']);
+    if (path.extname(sitesPath) === '.json') {
+      return JSON.parse(fs.readFileSync(sitesPath, 'utf-8'));
+    } else if (path.extname(sitesPath) === '.js') {
+      return eval(fs.readFileSync(sitesPath, 'utf-8'));
+    } else {
+      throw new Error('Must pass a js or json file to --sites-path');
+    }
   }
 
-  return SITES;
+  if (args['subset']) {
+    return eval(fs.readFileSync(path.resolve(__dirname, 'sites_subset.js'), 'utf-8'));
+  }
+
+  return eval(fs.readFileSync(path.resolve(__dirname, 'sites.js'), 'utf-8'));
 }
 
 const URLS = getUrls();
@@ -298,8 +199,8 @@ function singleRunAnalysis(url, id, launcher, {ignoreRun}) {
 function analyzeWithLighthouse(launcher, url, outputPath, assetsPath, {ignoreRun}) {
   const flags = {
     output: 'json',
-    disableCpuThrottling: args['disable-cpu-throttling'],
-    disableNetworkThrottling: args['disable-network-throttling'],
+    disableCpuThrottling: ignoreRun ? true : args['disable-cpu-throttling'],
+    disableNetworkThrottling: ignoreRun ? true : args['disable-network-throttling'],
     disableDeviceEmulation: args['disable-device-emulation'],
     port: launcher.port,
   };
